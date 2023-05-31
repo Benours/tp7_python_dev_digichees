e46b1f18a4c095d9eea0457a85edb10522d65449
Gillian Catil <gigicatil@hotmail.fr>
2023-05-31 16:29:12 +0200

    CRUD WeightTag et CRUD Conditioning

--- db/pony_entitites.py
+++ db/pony_entitites.py
@@ -110,7 +110,7 @@ class Weight(db.Entity):
 
 class WeightTag(db.Entity):
     _table_ = "t_weight_tag"
-    values = Required(float)
+    tagValues = Required(float)
     conditioning = Set(Conditioning)
 
 db.generate_mapping(create_tables=True)
