dfc44e04c749a36f2720db93b7372b91f30453e6
benours <benjamin.baska@gmail.com>
2023-05-31 10:37:19 +0200

    Add user CRUD

--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from src.CRUD_user import router as user_router
+import uvicorn
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app.include_router(user_router, prefix="/user", tags=["user"])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
