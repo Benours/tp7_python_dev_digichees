52bdad4793814df4f93a39e525ee50079359a0a3
Gillian Catil <gigicatil@hotmail.fr>
2023-06-01 16:34:34 +0200

    ajout de commentaire pour pydoc

--- tests/test_CRUD_weight_tag.py
+++ tests/test_CRUD_weight_tag.py
@@ -1,12 +1,14 @@
 import unittest
 from src.CRUD_weight_tag import *
-#La class permet de regrouper les tes à effectué
+#La class permet de regrouper les tests à effectué
 class test_Weight_Tag_Crud(unittest.TestCase):
     #Mais ne pas lancé le test testWeightTagCrud parce que
     #le lancement ce fait dans le désordre et ne créer pas
     #le PoidV avant de le supprimer ou le modifier.
     #Donc, il faut le lancer dans l'ordre la création d'abord
     #la suppression à la fin
+
+    # test de la création de poidV
     def test_Create(self):
         #la fonction createWeightTag retourne "successful creation"
         #si la method fonctionne correctement pour qu'on le voit sur swagger
@@ -23,6 +25,7 @@ class test_Weight_Tag_Crud(unittest.TestCase):
         #print(self.id)
         self.assertEquals(res[len(res)-1], (self.id,2.0))
 
+    # test pour avoir le poidV associé à l'id inscrit
     def test_Get_By_Id(self):
         #si l'ordre est respecté par incrémentation nous devons retrouver à la dernière place
         #par id.
@@ -32,6 +35,7 @@ class test_Weight_Tag_Crud(unittest.TestCase):
         res2=get_weight_tag_by_id(self.id)
         self.assertEquals(res2, [(self.id,2.0)])
 
+    # test pour modifier les informations associé à l'id
     def test_Update(self):
         #donc parce que on a besoin de l'id du dernier emplacement
         #on récupère l'id de getWeightTag pour l'update
@@ -43,6 +47,7 @@ class test_Weight_Tag_Crud(unittest.TestCase):
         res= res2[len(res2) - 1]
         self.assertEquals(res, (self.id, 6.0))
 
+    # test de suppression de l'id avec son contenu
     def test_Delete(self):
         #tout comme l'update, on a besoin de l'id du dernier emplacement pour supprimer
         #le PoidV test
