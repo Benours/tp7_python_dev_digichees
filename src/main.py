ade208dd2491aa33fa0c81603aa9927359f475b0
Gillian Catil <gigicatil@hotmail.fr>
2023-06-01 16:07:59 +0200

    correction

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ from src.CRUD_weight_tag import app as weight_tag_router
 from src.CRUD_city import app as city_router
 from src.CRUD_conditioning import app as conditioning_router
 from src.authentification.authtentification import router as authentification_router
+from src.Impress import app as impress_router
+
 import uvicorn
 
 # Create the swagger
@@ -19,9 +21,9 @@ app.include_router(weight_tag_router, prefix="/weightTag", tags=["weightTag"])
 app.include_router(city_router, prefix="/city", tags=["city"])
 app.include_router(conditioning_router, prefix="/conditioning", tags=["conditioning"])
 app.include_router(authentification_router, prefix="/authentification", tags=["authentification"])
+app.include_router(impress_router, prefix="/impress", tags=["impress"])
 
 # Launch uvicorn and access to swagger
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
-from src.Impress import app as impress_router
-app.include_router(impress_router, prefix="/impress", tags=["impress"])
+
