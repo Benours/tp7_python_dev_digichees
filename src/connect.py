1b482f8b1afe2fb6e0ba7c9fb49851bf4743f45b
Gillian Catil <gigicatil@hotmail.fr>
2023-06-01 16:03:23 +0200

    commit pour merge

--- src/connect.py
+++ src/connect.py
@@ -5,6 +5,6 @@ conn = mysql.connector.connect(
     host="localhost",
     port=3306,
     user="root",
-    password="",
+    password="root",
     database="digichees"
 )
--- src/main.py
+++ src/main.py
@@ -23,3 +23,4 @@ app.include_router(authentification_router, prefix="/authentification", tags=["a
 # Launch uvicorn and access to swagger
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
